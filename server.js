9c1f3aff4b4d25324d878840c9c54aaee0c589cb
Misra Turp <misraturp@gmail.com>
2019-12-14 21:50:58 +0100

    deploying

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post('/imageurl', (req,res) => {
 })
 
 app.listen(process.env.PORT || 3000, () =>{
-	console.log('app is running on port ${process.env.PORT}');
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
