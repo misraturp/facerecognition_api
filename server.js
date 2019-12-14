6260e10458b407a12bd6138f6edac9bc425c5d47
Misra Turp <misraturp@gmail.com>
2019-12-14 22:08:38 +0100

    added database on heroku url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-asymmetrical-18117',
     user : '',
     password : '',
     database : 'smart-brain'
