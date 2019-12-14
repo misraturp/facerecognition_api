408e9eac42c4ea11a87145dee4a877bbfa00d8b1
Misra Turp <misraturp@gmail.com>
2019-12-14 22:12:55 +0100

    connecting to database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-asymmetrical-18117',
-    user : '',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
